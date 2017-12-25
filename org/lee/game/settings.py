4eafd84952e3373ba5430e298753a0a3b487f671
Jason <angellee1988@qq.com>
2017-12-25 23:04:01 +0800

    绘图

---
+++ org/lee/game/settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env Python
+'#!/usr/bin/python'
+#coding=utf-8
+'''
+@author: xingjiang.li
+'''
+
+class Settings():
+    '''存储《外星人入侵》的所有设置的类'''
+
+    def __init__(self):
+        '''初始化游戏的设置'''
+        self.screen_width=1000
+        self.screen_height=500
+        self.bg_color = (230,230,230)
